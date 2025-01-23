539a368984e497e596ebc78a197714b9e74d7e7f
edpoffo <68963618+edpoffo@users.noreply.github.com>
2025-01-23 08:19:51 -0300

    Página de entrada de portfólio 70% pronta
    Só faltam os arquivos (Se tiver)

--- Js/portfolioFetch.js
+++ Js/portfolioFetch.js
@@ -5,11 +5,6 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const entry = urlParams.get("entry")
 
-var ProjectNameText = document.getElementById("Titulo");
-var projectDescriptionText = document.getElementById("desc");
-console.log(projectDescriptionText);
-
-
 console.log(entry);
 
 //if(entry == null) window.location.href = "index.html";
@@ -27,3 +22,34 @@ if(found == null) console.log("404");
 
 ////// Updating page
 /// Modifying page header
+var ProjectNameText = document.getElementById("Titulo");
+var projectDescriptionText = document.getElementById("desc");
+var mainPhoto = document.getElementById("mainPhoto");
+var photoArea = document.getElementById("photoArea");
+
+console.log(found.NomeFotoCapa);
+console.log(mainPhoto.src);
+
+
+ProjectNameText.textContent = found.Nome;
+projectDescriptionText.textContent = found.Descricao;
+mainPhoto.src = "../Portfolios/"+found.Nome+"/"+found.NomeFotoCapa;
+console.log(projectDescriptionText);
+
+
+// Add photo album
+for (let i = 0; i < found.Fotos.length; i++) {
+
+    const newPhotoRect = document.createElement("div");
+    newPhotoRect.setAttribute("class", "squarePhoto");
+    newPhotoRect.setAttribute("width", "20%");
+    
+    photoArea.append(newPhotoRect);
+    
+    const newPhoto = document.createElement("img");
+    var photoSrc = "../Portfolios/"+found.Nome+"/"+found.Fotos[i];
+    newPhoto.setAttribute("src", photoSrc);
+    newPhoto.setAttribute("class", "Photo");
+    
+    newPhotoRect.append(newPhoto);
+}
